9163ee23b04fc741e55d747cb55b9087d616e49f
RafaelD3v <rafaelcontato66738621+RafaelD3v@users.noreply.github.com>
2021-04-19 19:53:32 -0300

    Aula1

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,13 @@
+import '../home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/splash/splash_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../core/core.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear,
+        ),
+        child: Center(
+          child: Image.asset(
+            AppImages.logo,
+          ),
+        ),
+      ),
+    );
+  }
+}
