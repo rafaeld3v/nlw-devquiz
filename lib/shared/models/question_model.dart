0d4da790e18aee971fd666ac20d1e945cf5d177f
RafaelD3v <rafaelcontato.ce@gmail.com>
2021-05-20 17:08:01 -0300

    Aula 3

--- lib/shared/models/question_model.dart
+++ lib/shared/models/question_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:devquiz/shared/models/answered_model.dart';
 
 class QuestionModel {
@@ -8,4 +10,24 @@ class QuestionModel {
     required this.title,
     required this.answers,
   }) : assert(answers.length == 4);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'answers': answers.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory QuestionModel.fromMap(Map<String, dynamic> map) {
+    return QuestionModel(
+      title: map['title'],
+      answers: List<AwnserModel>.from(
+          map['answers']?.map((x) => AwnserModel.fromMap(x))),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuestionModel.fromJson(String source) =>
+      QuestionModel.fromMap(json.decode(source));
 }
