a079f00ea95200431fbccc94f3c90e223a0c83f6
RafaelD3v <rafaelcontato.ce@gmail.com>
2021-05-24 18:32:32 -0300

    Aula 4

--- lib/challenge/widgets/next_button/next_button_widget.dart
+++ lib/challenge/widgets/next_button/next_button_widget.dart
@@ -1,18 +1,62 @@
+import 'package:devquiz/core/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NextButtonWidget extends StatelessWidget {
   final String label;
-
+  final Color backgroundColor;
+  final Color fontColor;
+  final Color borderColor;
   const NextButtonWidget({
     Key? key,
     required this.label,
+    required this.backgroundColor,
+    required this.fontColor,
+    required this.borderColor,
   }) : super(key: key);
+
+  NextButtonWidget.green({required String label})
+      : this.backgroundColor = AppColors.darkGreen,
+        this.fontColor = AppColors.white,
+        this.borderColor = AppColors.white,
+        this.label = label;
+
+  NextButtonWidget.white({required String label})
+      : this.backgroundColor = AppColors.white,
+        this.fontColor = AppColors.grey,
+        this.borderColor = AppColors.lightGrey,
+        this.label = label;
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {},
-      child: Text(label),
+    return Container(
+      height: 48,
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(
+            backgroundColor,
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          side: MaterialStateProperty.all(
+            BorderSide(
+              color: borderColor,
+            ),
+          ),
+        ),
+        onPressed: () {},
+        child: Text(
+          label,
+          style: GoogleFonts.notoSans(
+            fontWeight: FontWeight.w600,
+            fontSize: 15,
+            color: fontColor,
+          ),
+        ),
+      ),
     );
   }
 }
