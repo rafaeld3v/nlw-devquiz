c0f9cd902b248a0e40c40a9fa7580c4233d85003
RafaelD3v <rafaelcontato.ce@gmail.com>
2021-05-24 18:06:42 -0300

    Aula 4

--- lib/challenge/widgets/next_button/next_button_widget.dart
+++ lib/challenge/widgets/next_button/next_button_widget.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
 class NextButtonWidget extends StatelessWidget {
+  final String label;
+
+  const NextButtonWidget({
+    Key? key,
+    required this.label,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextButton(
+      onPressed: () {},
+      child: Text(label),
+    );
   }
 }
